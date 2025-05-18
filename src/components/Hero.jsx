0737219a4e6dec55074fce1e77c1b9a0c8781773
bipulchhetri <bipulchhetri80@gmail.com>
2025-05-18 22:41:46 +0530

    hero  responsive udpate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,18 +22,49 @@
 
 'use client'
 
-import Image from 'next/image'
+
+
+// export default function Banner() {
+//   return (
+//     <div className="relative w-full h-[250px] sm:h-[350px] md:h-[500px] lg:h-[650px] xl:h-[750px]">
+//       <Image
+//         src="/banner1.jpg" // Place image in public/assets/img/slider/
+//         alt="Nalanda Learning"
+//         fill
+//         priority
+//         className="object-cover"
+//       />
+//     </div>
+//   )
+// }
+// import Image from 'next/image';
+
+// export default function Banner() {
+//   return (
+//     <div className="relative w-full h-[250px] sm:h-[350px] md:h-[500px] lg:h-[650px] xl:h-[750px]">
+//       <Image
+//         src="/banner1.jpg"
+//         alt="Nalanda Learning"
+//         fill
+//         priority
+//         className="object-cover w-full h-full"
+//       />
+//     </div>
+//   );
+// }
+import Image from 'next/image';
 
 export default function Banner() {
   return (
-    <div className="relative w-full h-[250px] sm:h-[350px] md:h-[500px] lg:h-[650px] xl:h-[750px]">
+    <div className="relative w-full aspect-[16/9] sm:aspect-[16/7] md:aspect-[16/6] lg:aspect-[16/5] xl:aspect-[16/4]">
       <Image
-        src="/banner1.jpg" // Place image in public/assets/img/slider/
+        src="/banner1.jpg"
         alt="Nalanda Learning"
         fill
         priority
         className="object-cover"
       />
     </div>
-  )
+  );
 }
+
